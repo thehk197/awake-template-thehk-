43ed3f3e17a61135a2620f70cb370184159269f0
theharshitkumar <harshit.hk.3012@gmail.com>
2020-06-10 00:17:38 +0530

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id:UA-168801679-1
+    id: process.env.UA-168801679-1
   },
   mainMenu: [
   {
