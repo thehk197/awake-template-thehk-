13b950f4205f149b0a3c887b4074d77b01bce452
theharshitkumar <harshit.hk.3012@gmail.com>
2020-06-11 17:55:30 +0530

    s

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form >
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
--- pages/contact.vue
+++ pages/contact.vue
@@ -40,7 +40,63 @@
           </div>
           <div class="tile is-parent is-8">
             <article class="tile is-child box">
-              <contact-form />
+  <form name="contact" method="POST" data-netlify="true">
+  <!-- Name -->
+    <div class="field">
+      <label class="label is-medium">Name</label>
+      <div class="control has-icons-left">
+        <input class="input is-medium" type="text" placeholder="John Smith" />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="user" />
+        </span>
+      </div>
+    </div>
+    <!-- Email -->
+    <div class="field">
+      <label class="label is-medium">Email</label>
+      <div class="control has-icons-left">
+        <input
+          class="input is-medium"
+          type="email"
+          placeholder="you@email.com"
+        />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="envelope" />
+        </span>
+      </div>
+    </div>
+    <!-- Subject -->
+    <div class="field">
+      <label class="label is-medium">Subject</label>
+      <div class="control has-icons-left">
+        <input
+          class="input is-medium"
+          type="text"
+          placeholder="Brief Summary"
+        />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="briefcase" />
+        </span>
+      </div>
+    </div>
+    <!-- Message -->
+    <div class="field">
+      <label class="label is-medium">Message</label>
+      <div class="control">
+        <textarea
+          class="textarea"
+          placeholder="Detailed description of your comment, request, etc"
+          rows="8"
+        ></textarea>
+      </div>
+    </div>
+	<div data-netlify-recaptcha="true"></div>
+	<div class="control">
+      <button class="button is-primary">
+        Contact Us
+      </button>
+    </div>
+</form>
             </article>
           </div>
         </div>
