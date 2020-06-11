f01bec4f888d0b401fff7dbb880ebbfcd9392478
theharshitkumar <harshit.hk.3012@gmail.com>
2020-06-11 15:19:34 +0530

    form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,5 +1,8 @@
 <template>
-  <form>
+  <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true" netlify-honeypot="bot-field">
+   <p class="hidden">
+    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+  </p>
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
@@ -50,7 +53,7 @@
       </div>
     </div>
     <div class="control">
-      <button class="button is-primary">
+      <button class="button is-primary" type="submit">
         Contact Us
       </button>
     </div>
